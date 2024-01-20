a0cb4e79c9835333bc9f98d33188010ae8cf1988
Degreat <mail@degreat.co.uk>
2024-01-20 08:00:40 +0000

    show loader

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,36 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import UnoCSS from 'unocss/astro';
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import UnoCSS from 'unocss/astro'
+import react from '@astrojs/react'
+import {
+  transformerNotationDiff,
+  transformerNotationHighlight,
+} from 'shikiji-transformers'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://degreat.co.uk',
-  integrations: [mdx(), sitemap(), UnoCSS({
-    injectReset: true
-  }), react()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    UnoCSS({
+      injectReset: true,
+    }),
+    react(),
+  ],
   markdown: {
     shikiConfig: {
       experimentalThemes: {
         light: 'github-light',
-        dark: 'vitesse-dark'
-      }
-    }
+        dark: 'vitesse-dark',
+      },
+      transformers: [transformerNotationDiff(), transformerNotationHighlight()],
+    },
   },
   vite: {
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js-darwin-arm64']
-    }
-  }
-});
+      exclude: ['@resvg/resvg-js-darwin-arm64'],
+    },
+  },
+})
