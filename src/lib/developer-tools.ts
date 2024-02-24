5374f244b4146fc7ddc0cd28790e4edca3ca532b
Degreat <mail@degreat.co.uk>
2024-02-24 08:47:09 +0000

    rearrange

--- src/lib/developer-tools.ts
+++ src/lib/developer-tools.ts
@@ -1,15 +1,15 @@
 const developerTools = [
   {
-    title: 'DotLocal',
-    description:
-      'Mirror your production domains on your .local machine',
-    link: 'https://degreat.co.uk/dotlocal',
-  },
-  {
     title: 'Many Things',
     description:
       'Track tasks you need to work on and quickly list ideas right from the menu bar.',
     link: 'https://github.com/blackmann/ManyThings'
+  },
+  {
+    title: 'DotLocal',
+    description:
+      'Mirror your production domains on your .local machine',
+    link: 'https://degreat.co.uk/dotlocal',
   },
   {
     title: 'Wipe_Modules',
