14f629156516023c2a9085c5ad570b91fe72b890
Degreat <mail@degreat.co.uk>
2024-02-26 16:59:03 +0000

    smol

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'GR'
+export const SITE_TITLE = "GR";
 export const SITE_DESCRIPTION =
-  'Home of the Super Creative 🏄🏽. Find my writings, projects, courses and artworks here.'
-export const SITE_HOST = 'degreat.co.uk'
+	"Home of the Super Creative 🏄🏽. Find my writings, projects, courses and artworks here.";
+export const SITE_HOST = "degreat.co.uk";
--- src/lib/developer-tools.ts
+++ src/lib/developer-tools.ts
@@ -3,19 +3,19 @@ const developerTools = [
     title: 'Many Things',
     description:
       'Track tasks you need to work on and quickly list ideas right from the menu bar.',
-    link: 'https://github.com/blackmann/ManyThings'
+    url: 'github.com/blackmann/ManyThings'
   },
   {
     title: 'DotLocal',
     description:
       'Mirror your production domains on your .local machine',
-    link: 'https://degreat.co.uk/dotlocal',
+    url: 'degreat.co.uk/dotlocal',
   },
   {
     title: 'Wipe_Modules',
     description:
       'Clean stale node_modules from old projects and reclaim disk space. Mac only.',
-    link: 'https://github.com/blackmann/Wipe_Modules',
+    url: 'github.com/blackmann/Wipe_Modules',
   },
 ]
 
