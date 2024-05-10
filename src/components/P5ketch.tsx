dd91c89171509b19cae00701325535382faafd81
Degreat <mail@degreat.co.uk>
2024-05-10 17:23:19 +0000

    progress

--- src/components/P5ketch.tsx
+++ src/components/P5ketch.tsx
@@ -3,25 +3,24 @@ import { javascript } from "@codemirror/lang-javascript";
 import { keymap } from "@codemirror/view";
 import clsx from "clsx";
 import { EditorView, basicSetup } from "codemirror";
+import p5 from "p5";
 import * as babel from "prettier/plugins/babel.js";
 import * as estree from "prettier/plugins/estree.js";
 import prettier from "prettier/standalone";
 import React from "react";
 import "../styles/sketch-editor.css";
 
-const template = `function setup() {
+const CANVAS_BASE_WIDTH = 1080;
+const TEMPLATE = `function setup() {}
 
-}
-
-function draw() {
-
-}
+function draw() {}
 `;
 
 function P5ketch() {
+	const canvasRef = React.useRef<HTMLDivElement>(null);
 	const editorRef = React.useRef<HTMLDivElement>(null);
-	const [editorView, setEditorView] = React.useState<EditorView>();
 
+	const [editorView, setEditorView] = React.useState<EditorView>();
 	const [applied, setApplied] = React.useState("");
 	const [lastCode, setLastCode] = React.useState("");
 
@@ -72,7 +71,7 @@ function P5ketch() {
 		});
 
 		const view = new EditorView({
-			doc: template,
+			doc: TEMPLATE,
 			extensions: [
 				basicSetup,
 				javascript(),
@@ -93,18 +92,112 @@ function P5ketch() {
 			return;
 		}
 
-		const lastCode = localStorage.getItem("last_code") || template;
+		const lastCode = localStorage.getItem("last_code") || TEMPLATE;
 		setApplied(lastCode);
 		editorView.dispatch({
 			changes: { from: 0, to: editorView.state.doc.length, insert: lastCode },
 		});
 	}, [editorView]);
 
+	React.useEffect(() => {
+		try {
+			let resize: () => void;
+			let scale = 1;
+
+			const p = new p5((sketch) => {
+				sketch.grid = () => {
+					sketch.push();
+					sketch.stroke("#ddd");
+					let x = 40;
+					const h = (CANVAS_BASE_WIDTH * 7) / 16;
+					while (x < CANVAS_BASE_WIDTH) {
+						sketch.line(x, 0, x, h);
+
+
+						sketch.push();
+
+						sketch.fill("#aaa");
+						sketch.noStroke();
+						sketch.text(`${x / 40}`, x, 10);
+
+						sketch.pop();
+
+						x += 40;
+					}
+
+					let y = 40;
+					while (y < h) {
+						sketch.line(0, y, CANVAS_BASE_WIDTH, y);
+
+						sketch.push();
+
+						sketch.fill("#aaa");
+						sketch.noStroke();
+						sketch.textAlign(sketch.RIGHT, sketch.CENTER);
+						sketch.text(`${y / 40}`, 35, y + 8);
+
+						sketch.pop();
+
+						y += 40;
+					}
+
+					sketch.pop();
+
+
+				};
+
+				const f = new Function("sketch", `${applied};return {draw, setup}`);
+				const { setup, draw } = f(sketch);
+
+				sketch.setup = () => {
+					if (!canvasRef.current) return;
+
+					const { clientHeight, clientWidth } = canvasRef.current;
+					const canvas = sketch.createCanvas(clientWidth, clientHeight);
+
+					scale = clientWidth / CANVAS_BASE_WIDTH;
+					canvas.style.width = clientWidth;
+					canvas.style.height = clientHeight;
+
+					resize = () => {
+						if (!canvasRef.current) return;
+
+						const { clientHeight, clientWidth } = canvasRef.current;
+						sketch.resizeCanvas(clientWidth, clientHeight);
+						canvas.style.width = clientWidth;
+						canvas.style.height = clientHeight;
+
+						scale = clientWidth / CANVAS_BASE_WIDTH;
+					};
+
+					window.addEventListener("resize", resize);
+
+					setup();
+				};
+
+				sketch.draw = () => {
+					sketch.scale(scale);
+					draw();
+				};
+			}, canvasRef.current);
+
+			return () => {
+				p.remove();
+				window.removeEventListener("resize", resize);
+			};
+		} catch (err) {
+			console.error(err);
+		}
+	}, [applied]);
+
 	const hasChanges = applied !== lastCode;
 
 	return (
 		<div>
-			<canvas className="block aspect-[16/7] w-full rounded-lg border" />
+			<div
+				className="block aspect-[16/7] w-full rounded-lg border overflow-hidden"
+				ref={canvasRef}
+			/>
 
 			<div className="grid grid-cols-1 md:grid-cols-3 mt-2">
 				<div className="grid-cols-1 md:col-span-2">
