f13f7ce2682740db09dd7c75258c99d88e4e0c0d
Degreat <mail@degreat.co.uk>
2024-05-20 15:06:54 +0000

    image select

--- src/components/InstaxForm.tsx
+++ src/components/InstaxForm.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import Cropper from "react-easy-crop";
+
 const hostels = [
 	"New Brunei",
 	"Brunei Complex",
@@ -15,11 +18,69 @@ const hostels = [
 ];
 
 function InstaxForm() {
+	const [image, setImage] = React.useState<any>(null);
+	const [crop, setCrop] = React.useState<any>({ x: 0, y: 0 });
+	const [zoom, setZoom] = React.useState(1);
+	const [rotation, setRotation] = React.useState(0);
+
+	function handleImageSelect(event: React.ChangeEvent<HTMLInputElement>) {
+		const file = event.target.files?.[0];
+		if (!file) return;
+		const reader = new FileReader();
+
+		reader.onload = () => {
+			setImage(reader.result);
+		};
+
+		reader.readAsDataURL(file);
+	}
+
 	return (
-		<form>
+		<form className="relative">
 			<div className="bg-zinc-50 rounded aspect-[2/3] p-4 shadow-lg">
-				<div className="bg-zinc-800 aspect-[3/4] rounded" />
+				{!image && (
+					<label htmlFor="image">
+						<div className="bg-zinc-800 aspect-[3/4] rounded flex justify-center items-center">
+							<div className="flex items-center justify-center rounded-full bg-zinc-200 w-[8rem] aspect-square text-center font-bold leading-none pulsate">
+								Tap here <br />
+								to select
+								<br /> an image
+							</div>
+						</div>
+					</label>
+				)}
+
+				{image && (
+					<>
+						<Cropper
+							aspect={3 / 4}
+							onCropChange={setCrop}
+							image={image}
+							crop={crop}
+							zoom={zoom}
+							onZoomChange={setZoom}
+							rotation={rotation}
+							objectFit="contain"
+							onRotationChange={setRotation}
+							classes={{
+								containerClassName: "m-4 aspect-[3/4] rounded border",
+							}}
+						/>
+					</>
+				)}
 			</div>
+
+			{image && (
+				<div className="flex gap-2 mt-2 text-secondary">
+					<div className="i-lucide-ticket-x" />
+					<div className="leading-none flex-1 text-sm">
+						Not the image?{" "}
+						<label htmlFor="image" className="font-medium text-blue-500">
+							Click here to change image
+						</label>
+					</div>
+				</div>
+			)}
 
 			<div className="flex gap-2 mt-2 text-secondary">
 				<div className="i-lucide-scan-line" />
@@ -29,17 +90,22 @@ function InstaxForm() {
 				</div>
 			</div>
 
+			<div className="flex gap-2 mt-2 text-secondary">
+				<div className="i-lucide-film" />
+				<div className="leading-none flex-1 text-sm">Print is 54mm x 86mm</div>
+			</div>
+
 			<div className="flex gap-2 mt-4">
 				<button
 					type="button"
-					className="font-mono font-bold bg-green-500 text-white rounded-lg px-1 py-1"
+					className="font-mono bg-green-500 text-white rounded-lg px-1 py-1"
 				>
 					1<span className="opacity-70">x</span> GHS50
 				</button>
 
 				<button
 					type="button"
-					className="font-mono font-bold bg-zinc-200 dark:bg-neutral-800 rounded-lg px-1 py-1"
+					className="font-mono bg-zinc-200 dark:bg-neutral-800 rounded-lg px-1 py-1"
 				>
 					2<span className="opacity-70">x</span> GHS90
 				</button>
@@ -92,7 +158,13 @@ function InstaxForm() {
 			>
 				Place Order
 			</button>
-			{/* <input type="file" /> */}
+			<input
+				type="file"
+				accept="image/jpg,image/jpeg,image/png"
+				className="absolute top-[10rem] opacity-0 left-[1rem]"
+				id="image"
+				onChange={handleImageSelect}
+			/>
 		</form>
 	);
 }
